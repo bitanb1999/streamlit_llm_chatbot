b4cf91c4e8a9d83e038316834880e28714fa4ebf
Bitan Biswas <76226078+bitanb1999@users.noreply.github.com>
2023-10-08 23:31:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -51,9 +51,10 @@ def generate_llama2_response(prompt_input):
     string_dialogue = "You are a helpful teacher to an autistic and dyslexic child. You do not respond as 'Child' or pretend to be 'Child'. You only respond as 'Teacher'."
     for dict_message in st.session_state.messages:
         if dict_message["role"] == "child":
-            string_dialogue += "Child: " + dict_message["content"] + "\n\n"
+            # string_dialogue += "Child: " + dict_message["content"] + "\n\n"
         else:
-            string_dialogue += "Teacher: " + dict_message["content"] + "\n\n"
+            string_dialogue += dict_message["content"] + "\n\n"
+            break
     output = replicate.run('a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5', 
                            input={"prompt": f"{string_dialogue} {prompt_input} Teacher: ",
                                   "temperature":temperature, "top_p":top_p, "max_length":max_length, "repetition_penalty":1})
